5244a4bffab7ad4e4f6e9502b3c6ff816c4cab69
David Bobritsky <dvdbobr@gmail.com>
2021-03-04 11:19:09 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+const proxy = 'https://api.allorigins.win/raw?url=';
+const coutriesEndpoint = 'https://restcountries.herokuapp.com/api/v1';
+const covidStats = 'https://corona-api.com/countries';
+const continents = document.querySelector('.continents');
+//general get api function
+async function getApiData(url) {
+    let callApi = await fetch(url);
+    let data = await callApi.json();
+    return data;
+}
+//get all countries
+async function getCountries() {
+    let dataArr = await getApiData(`${proxy}${coutriesEndpoint}`);
+    console.log(dataArr)
+}
+//getCountries()
+//get object with name and continet of coutry 
+async function getCountryByContinent(continent) {
+    let dataArr = await getApiData(`${proxy}${coutriesEndpoint}`);
+    //console.log(dataArr)
+    let countryByContinentArr=[];
+    let countryByContinent = await Promise.all(
+        dataArr.map(async (c) => {
+            //console.log(c.region)
+            if (c.region === continent) {
+                countryByContinentArr.push({name: c.name.common, continent: c.region})
+            }
+        })
+    )
+    console.log(countryByContinentArr)
+}
+
+
+
+async function getCovidStats() {
+    let data = await getApiData(`${proxy}${covidStats}`);
+    let dataArr = data.data
+    //console.log(dataArr)
+    let covidInfo = await Promise.all(
+        dataArr.map(async (c) => {
+            return {
+                name: c.name, confirmed: c.latest_data.confirmed, deaths: c.latest_data.deaths,
+                recovered: c.latest_data.recovered, critical: c.latest_data.critical
+            }
+        })
+    )
+    //console.log(covidInfo)
+    return covidInfo;
+}
+getCovidStats()
+
+continents.addEventListener('click',async (e) => {
+    await getCountryByContinent(e.target.classList[0])
+});
